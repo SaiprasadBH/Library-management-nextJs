3d1fb285ab6be388ea6e90f68ca3ce8bd9065f64
Saiprsad <saiprasadhegde192@gmail.com>
2024-09-19 10:06:32 +0530

    fixing transaction repo issue

--- library-management-nextjs/src/lib/repositories/transaction.repository.ts
+++ library-management-nextjs/src/lib/repositories/transaction.repository.ts
@@ -48,7 +48,13 @@ export class TransactionRepository
       const createdTransaction: ITransaction = await db.transaction(
         async (tx) => {
           const [result] = await tx
-            .select()
+            .select({
+              id: transactions.id,
+              memberId: transactions.memberId,
+              bookId: transactions.bookId,
+              bookStatus: transactions.bookStatus,
+              dateOfIssue: transactions.dateOfIssue,
+            })
             .from(transactions)
             .where(eq(transactions.bookId, validatedData.bookId));
           await tx.insert(transactions).values(newTransaction as ITransaction);
